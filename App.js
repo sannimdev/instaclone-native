d654b8ba839ff308eefacd7344a1b904782eb8ca
sannimdev <inegg.apps@gmail.com>
2022-02-14 20:19:24 +0900

    13.6 AppLoading

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [loading, setLoading] = useState(true);
+    const onFinish = () => setLoading(false);
+    const preload = () => {
+        // 항사 ㅇpromise를 리턴해야 한다.
+        const fontsToLoad = [Ionicons.font];
+        const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+        console.log(fontPromises);
+        return Promise.all([fontPromises]);
+    };
+    if (loading) {
+        return <AppLoading startAsync={preload} onError={console.warn} onFinish={onFinish} />;
+    }
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!!!!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ README.md
@@ -0,0 +1,11 @@
+# README
+
+## 13.6
+
+```
+expo install expo-app-loading
+```
+
+```
+expo install expo-font
+```
